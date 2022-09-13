8bf3a04d2de62c1390c8bc17346d35f337d75b84
Phan Hùng Phong <phongphan@heallios.com>
2022-09-13 14:20:29 +0700

    design footer by phan tấn phú

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register/Register";
 import "./App.scss";
 import Home from "./pages/Home/Home";
+import Footter from "./components/Footer/Footer";
 
 function App() {
   const admin = localStorage.getItem("admin");
@@ -11,7 +12,7 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Register />} />
+          <Route path="/" element={<Footter />} />
 
           {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
