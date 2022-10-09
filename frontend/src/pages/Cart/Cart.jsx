0d81bfa649a67a302f4f2c46a80e58eac7cce69b
phantanphu1 <phantanphu1505@gmail.com>
2022-10-09 18:09:47 +0700

    handle cart

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Cart.scss";
 import Navbar from "../../components/Navbar/Navbar";
+import axios from "axios";
+import { useLocation, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
 
 const Cart = () => {
+
+  const [data, setData] = useState([]);
+  const user = JSON.parse(localStorage.getItem("dataUser"));
+  const location = useLocation();
+  const navigate = useNavigate();
+  const handleMoveBuy = (e) => {
+    navigate(`/productDetail/payment/${e}`);
+  };
+
+  const handleDeleteCart = async (e) => {
+    await axios
+      .delete(`http://localhost:8000/api/cart/${e}`)
+      .then(function (response) {
+        toast.success("Xóa sản phẩm thành công ", {
+          position: toast.POSITION.BOTTOM_LEFT
+        });
+
+      })
+      .catch(function (error) {
+        console.log(error)
+        toast.error("Xóa sản phẩm thất bại", {
+          position: toast.POSITION.BOTTOM_LEFT
+        });
+      })
+    await fetchData();
+  }
+
+  const fetchData = () => {
+    axios
+      .get(`http://localhost:8000/api/user/${user._id}`)
+      .then(function (response) {
+        setData(response.data.cart);
+        console.log(response.data.cart)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    fetchData();
+
+  }, []);
   return (
     <div>
       <Navbar />
@@ -20,21 +67,21 @@ const Cart = () => {
             </div>
             <div className="container_cart_user_card_img_text">
               <div className="container_cart_user_card_img_text_name">
-                <p>NgoSonTung</p>
-                <p>user</p>
+                <p>{user.username}</p>
+                <p>{user.admin === false && <span>user</span>}</p>
               </div>
               <div className="container_cart_user_card_information">
                 <div className="container_cart_user_card_information_text">
                   <b>Email:</b>
-                  <i>ngosontung0309@gmail.com</i>
+                  <i>{user.email}</i>
                 </div>
                 <div className="container_cart_user_card_information_text">
                   <b>số điện thoại:</b>
-                  <i>012345678</i>
+                  <i>{user.numberPhone}</i>
                 </div>
                 <div className="container_cart_user_card_information_text">
                   <b>Địa chỉ:</b>
-                  <i>225/10 Nguyễn phước nguyên</i>
+                  <i>{user.address}</i>
                 </div>
               </div>
             </div>
@@ -46,36 +93,43 @@ const Cart = () => {
             <h4>Giỏ Hàng của bạn</h4>
           </div>
           <div className="container_cart_user_right_table">
-          <table>
-            <tr>
-              <th>Hình ảnh</th>
-              <th>Tên sản phẩm</th>
-              <th>các Size</th>
-              <th>các Màu</th>
-              <th>Giá bán</th>
-              <th>Chức năng</th>
-            </tr>
-            <tr>
-              <td className="cart_product">
-                <img
-                  src="https://product.hstatic.net/200000260587/product/b6cab97b491cbc42e50d_6d85a7f0498b4442832b5c0e3789bec0_grande.jpg"
-                  alt=""
-                />
-              </td>
-              <td> HARD MODE BASIC TEE / BEIGE</td>
-              <td>S,M,X,XXL</td>
-              <td>Đỏ,trắng,đen,nau</td>
-              <td style={{ color: "#d63031", fontWeight: "600" }}>
-                      69000₫</td>
-              <td className="cart_btn">
-                <button>Xem chi tiết</button>
-                <button>Xóa</button>
-              </td>
-            </tr>
-          </table>
+            <table>
+              <tr>
+                <th>Hình ảnh</th>
+                <th>Tên sản phẩm</th>
+                <th>các Size</th>
+                <th>các Màu</th>
+                <th>Giá bán</th>
+                <th>Chức năng</th>
+              </tr>
+              {data.map((item) => (
+                <tr>
+                  <td className="cart_product">
+                    <img
+                      src={item.Image}
+                      alt=""
+                    />
+                  </td>
+                  <td> {item.NameProduct}</td>
+                  <td>{item.Size}</td>
+                  <td>{item.Color}</td>
+                  <td style={{ color: "#d63031", fontWeight: "600" }}>
+                    {item.price}</td>
+                  <td className="cart_btn">
+                    <button onClick={()=>{handleMoveBuy(item.ProductID)}}>Xem chi tiết</button>
+                    <button onClick={() => {
+                      handleDeleteCart(item._id)
+                    }}>Xóa</button>
+                  </td>
+                </tr>
+              ))}
+            </table>
           </div>
         </div>
       </div>
+      <ToastContainer
+        autoClose={500}
+      />
     </div>
   );
 };
